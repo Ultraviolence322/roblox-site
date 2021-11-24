aca3f6e7c3d8839f443dcff453c7953d5d1626ad
Ultraviolence322 <segeichenkov18@gmail.com>
2021-11-24 10:12:56 +0300

    added tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,7 +45,7 @@ const Home: NextPage = () => {
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
+            <h2 className="text-red-300">Deploy &rarr;</h2>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
