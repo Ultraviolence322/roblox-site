7596204d1e9fc364a0f31a7b4496f41fe7f8654d
Ultraviolence322 <segeichenkov18@gmail.com>
2021-11-25 11:11:40 +0300

    added pages

---
+++ pages/artists.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from 'next'
+import React from 'react'
+
+interface Props {
+  
+}
+
+const Artists: NextPage<Props> = () => {
+  return (
+    <div>
+      <h1>Artists</h1>
+    </div>
+  )
+}
+
+export default Artists
---
+++ pages/new.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { NextPage } from 'next'
+
+interface Props {
+
+}
+
+const New: NextPage<Props> = () => {
+  return (
+    <div>
+      <h1>New Songs</h1>
+    </div>
+  )
+}
+
+export default New
