7210922d3f7e17b911af2a5d912a96b507a1d518
Ultraviolence322 <segeichenkov18@gmail.com>
2021-11-26 11:26:30 +0300

    updated main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
 import type { GetStaticProps, NextPage } from 'next'
-import { useEffect, useState } from 'react'
 
 import { IParsedSong } from '../types/ISong'
 
-import Paginator from '../components/Paginator'
-import TracksGrid from '../components/TracksGrid'
-import Search from '../components/Search'
 import { fetchAllSongs } from '../helpers/fetchAllSongs'
+
+import GridNavigate from '../components/GridNavigate'
 
 interface Props {
   parsedSongs: IParsedSong[],
@@ -16,53 +14,9 @@ interface Props {
 //artists, top(default), new(sort by code), search
 
 const Home: NextPage<Props> = ({parsedSongs, accessToken}) => {
-  const [currentSongs, setCurrentSongs] = useState(parsedSongs)
-  const [countSongsToShow, setCountSongsToShow] = useState(12)
-  const [currentPage, setCurrentPage] = useState(0)
-  const [countOfPages, setCountOfPages] = useState(Math.floor(parsedSongs.length / countSongsToShow))
-  const [songsToShow, setSongsToShow] = useState<IParsedSong[]>([])
-
-  useEffect(() => {
-    const fetchIdForSongs = async () => {
-      let songsWithId: IParsedSong[] = [] 
-      const songsToFetch = currentSongs
-        .slice(currentPage * countSongsToShow, (currentPage * countSongsToShow) + countSongsToShow)
-
-      await Promise.all(songsToFetch.map(async (s) => {
-        try {
-          const responseFromSpotify = await fetch(`https://api.spotify.com/v1/search?q=${s.songName}&type=track`, {
-            headers: {
-              Accept: "application/json",
-              Authorization: `Bearer ${accessToken}`,
-              "Content-Type": "application/json"
-            }
-          })
-
-          const dataFromSpotify = await responseFromSpotify.json()
-
-          songsWithId = [...songsWithId, {
-            ...s,
-            id: dataFromSpotify.tracks.items[0].id ? dataFromSpotify.tracks.items[0].id : null,
-          }]
-        } catch (error) {
-          console.log('error', error);
-        }
-      }))  
-
-      setSongsToShow([...songsWithId])
-      setCountOfPages(Math.floor(currentSongs.length / countSongsToShow))
-    }
-    
-    fetchIdForSongs()
-  }, [currentPage, countSongsToShow, currentSongs])
 
   return (
-    <div className="mx-auto max-w-5xl">
-      {currentSongs.length}
-      <Search items={parsedSongs} searchKey={'songName'} setSearchedItems={setCurrentSongs} />
-      <TracksGrid songsToShow={songsToShow} />
-      <Paginator countOfPages={countOfPages} currentPage={currentPage} setCurrentPage={setCurrentPage}/>
-    </div>
+    <GridNavigate parsedSongs={parsedSongs} accessToken={accessToken}/>
   )
 }
 
@@ -72,10 +26,9 @@ export const getStaticProps: GetStaticProps = async ( ) => {
 
   const responseFetchAccessToken = await fetch(`${process.env.DOMEN}/api/access-token`)
   const dataFetchAccessToken = await responseFetchAccessToken.json()
-  
   return { 
     props: { parsedSongs, accessToken: dataFetchAccessToken.accessToken },
-    revalidate: 3600,
+    revalidate: 3500,
   }
 }
 
