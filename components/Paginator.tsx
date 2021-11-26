670505bc706f63e2756107ebf8ed39d79c4b41d6
Ultraviolence322 <segeichenkov18@gmail.com>
2021-11-26 11:24:10 +0300

    upadated Paginator

--- components/Paginator.tsx
+++ components/Paginator.tsx
@@ -9,6 +9,90 @@ interface Props {
 
 const Paginator: NextPage<Props> = ({countOfPages, currentPage, setCurrentPage}) => {
 
+  const renderSteps = (countOfPages: number, currentPage:number) => {
+    if(countOfPages > 5) {
+      if(currentPage < 3) {
+        return new Array(countOfPages).fill(0).map((i, index) => {
+          if(index === countOfPages - 2) {
+            return <li 
+              className={PainatorItemClassName(index)}
+              key={index} 
+            >
+              ...
+            </li>
+          }
+          if(index < 3 || index === countOfPages - 1) {
+            return <li 
+              className={PainatorItemClassName(index)}
+              key={index} 
+              onClick={() => setCurrentPage(index)}
+            >
+              {index + 1}
+            </li>
+          }
+        })
+      } else if (currentPage >= 3 && currentPage <= countOfPages - 4) {
+        return new Array(countOfPages).fill(0).map((i, index) => {
+          if(index === 1) {
+            return <li 
+              className={PainatorItemClassName(index)}
+              key={index} 
+            >
+              ...
+            </li>
+          }
+          if(index === countOfPages - 2) {
+            return <li 
+              className={PainatorItemClassName(index)}
+              key={index} 
+            >
+              ...
+            </li>
+          }
+          if(currentPage === index || index === countOfPages - 1 || index === 0) {
+            return <li 
+              className={PainatorItemClassName(index)}
+              key={index} 
+              onClick={() => setCurrentPage(index)}
+            >
+              {index + 1}
+            </li>
+          }
+        })
+      } else {
+        return new Array(countOfPages).fill(0).map((i, index) => {
+          if(index === 1) {
+            return <li 
+              className={PainatorItemClassName(index)}
+              key={index} 
+            >
+              ...
+            </li>
+          }
+          if(index >= countOfPages - 3 || index === 0) {
+            return <li 
+              className={PainatorItemClassName(index)}
+              key={index} 
+              onClick={() => setCurrentPage(index)}
+            >
+              {index + 1}
+            </li>
+          }
+        })
+      }
+    } else {
+      return new Array(countOfPages).fill(0).map((i, index) => {
+        return <li 
+          className={PainatorItemClassName(index)}
+          key={index} 
+          onClick={() => setCurrentPage(index)}
+        >
+          {index + 1}
+        </li>
+      })
+    }
+  }
+
   const PainatorItemClassName = (currentIndex: number): string => {
     let className = `
       border-2 rounded-md
@@ -28,18 +112,40 @@ const Paginator: NextPage<Props> = ({countOfPages, currentPage, setCurrentPage})
   }
 
   return (
-    <ul className="flex flex-wrap pt-4">
-      {
-        new Array(countOfPages).fill(0).map((i, index) => {
-          return <li 
-            className={PainatorItemClassName(index)}
-            key={index} 
-            onClick={() => setCurrentPage(index)}
-          >
-            {index + 1}
-          </li>
-        })
-      }
+    <ul className="flex flex-wrap pt-4 justify-center">
+      <li 
+        className={`
+          border-2 rounded-md
+          w-10 h-10
+          m-1
+          text-center leading-9
+          cursor-pointer
+          hover:border-red-400
+        `}
+        onClick={() => {
+          setCurrentPage(--currentPage)
+        }}
+      >
+        left
+      </li>
+        
+      {renderSteps(countOfPages, currentPage)}
+
+      <li 
+        className={`
+          border-2 rounded-md
+          w-10 h-10
+          m-1
+          text-center leading-9
+          cursor-pointer
+          hover:border-red-400
+        `}
+        onClick={() => {
+          setCurrentPage(++currentPage)
+        }}
+      >
+        right
+      </li>
     </ul>
   )
 }
