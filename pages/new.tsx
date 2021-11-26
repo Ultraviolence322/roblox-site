b5e5cb8274f118e51543d4454fcb1b968f5a5b6f
Ultraviolence322 <segeichenkov18@gmail.com>
2021-11-26 14:51:30 +0300

    updated New Page

--- pages/new.tsx
+++ pages/new.tsx
@@ -1,16 +1,37 @@
 import React from 'react'
-import { NextPage } from 'next'
+import { GetStaticProps, NextPage } from 'next'
+
+import { fetchAllSongs } from '../helpers/fetchAllSongs'
+
+import { IParsedSong } from '../types/ISong'
+import GridNavigate from '../components/GridNavigate'
 
 interface Props {
-
+  parsedSongs: IParsedSong[],
+  accessToken: string
+}
+const New: NextPage<Props> = ({parsedSongs, accessToken}) => {
+  return (
+    <>
+    <h1>NEw</h1>
+    <GridNavigate parsedSongs={parsedSongs} accessToken={accessToken}/>
+    </>
+  )
 }
 
-const New: NextPage<Props> = () => {
-  return (
-    <div>
-      <h1>New Songs</h1>
-    </div>
-  )
+export const getStaticProps: GetStaticProps = async ( ) => {
+  let parsedSongs = await fetchAllSongs()
+  
+  parsedSongs = parsedSongs.sort((a, b) => {
+    return b.songCode - a.songCode
+  })
+
+  const responseFetchAccessToken = await fetch(`${process.env.DOMEN}/api/access-token`)
+  const dataFetchAccessToken = await responseFetchAccessToken.json()
+  return { 
+    props: { parsedSongs, accessToken: dataFetchAccessToken.accessToken },
+    revalidate: 3500,
+  }
 }
 
 export default New
