74aa9d6554f161f1e786a33f39c7e84b054f3cd2
Ultraviolence322 <segeichenkov18@gmail.com>
2021-11-26 11:24:30 +0300

    added component GridNavigate

---
+++ components/GridNavigate.tsx
@@ -0,0 +1,64 @@
+import { NextPage } from 'next'
+import React, { useEffect, useState } from 'react'
+import { IParsedSong } from '../types/ISong'
+import Paginator from './Paginator'
+import Search from './Search'
+import TracksGrid from './TracksGrid'
+
+interface Props {
+  parsedSongs: IParsedSong[],
+  accessToken: string
+}
+
+const GridNavigate: NextPage<Props> = ({parsedSongs, accessToken}) => {
+  const [currentSongs, setCurrentSongs] = useState(parsedSongs)
+  const [countSongsToShow, setCountSongsToShow] = useState(12)
+  const [currentPage, setCurrentPage] = useState(0)
+  const [countOfPages, setCountOfPages] = useState(Math.floor(parsedSongs.length / countSongsToShow))
+  const [songsToShow, setSongsToShow] = useState<IParsedSong[]>([])
+
+  useEffect(() => {
+    const fetchIdForSongs = async () => {
+      let songsWithId: IParsedSong[] = [] 
+      const songsToFetch = currentSongs
+        .slice(currentPage * countSongsToShow, (currentPage * countSongsToShow) + countSongsToShow)
+
+      await Promise.all(songsToFetch.map(async (s) => {
+        try {
+          const responseFromSpotify = await fetch(`https://api.spotify.com/v1/search?q=${s.songName}&type=track`, {
+            headers: {
+              Accept: "application/json",
+              Authorization: `Bearer ${accessToken}`,
+              "Content-Type": "application/json"
+            }
+          })
+
+          const dataFromSpotify = await responseFromSpotify.json()
+
+          songsWithId = [...songsWithId, {
+            ...s,
+            id: dataFromSpotify.tracks.items[0].id ? dataFromSpotify.tracks.items[0].id : null,
+          }]
+        } catch (error) {
+          console.log('error', error);
+        }
+      }))  
+
+      setSongsToShow([...songsWithId])
+      setCountOfPages(Math.floor(currentSongs.length / countSongsToShow))
+    }
+    
+    fetchIdForSongs()
+  }, [currentPage, countSongsToShow, currentSongs])
+
+  return (
+    <div className="mx-auto max-w-5xl">
+      {currentSongs.length}
+      <Search items={parsedSongs} searchKey={'songName'} setSearchedItems={setCurrentSongs} />
+      <TracksGrid songsToShow={songsToShow} />
+      <Paginator countOfPages={countOfPages} currentPage={currentPage} setCurrentPage={setCurrentPage}/>
+    </div>
+  )
+}
+
+export default GridNavigate
