f71bf27e5a74f921fcd4a1601624eb86aaa29c64
Ultraviolence322 <segeichenkov18@gmail.com>
2021-11-26 14:49:08 +0300

    updated search component

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,24 +1,36 @@
 import { NextPage } from 'next'
-import React, { Dispatch, FormEventHandler, SetStateAction, useState } from 'react'
-import { IParsedSong } from '../types/ISong'
+import React, { Dispatch, FormEventHandler, SetStateAction, useRef, useState } from 'react'
 
 interface Props {
   items: any[]
   searchKey: string
-  setSearchedItems: Dispatch<SetStateAction<any>>
+  setSearchedItems: Dispatch<SetStateAction<any[]>>
+  setCurrentPage?: Dispatch<SetStateAction<number>>
 }
-  const Search: NextPage<Props> = ({items, searchKey, setSearchedItems}) => {
+
+const Search: NextPage<Props> = ({items, searchKey, setSearchedItems, setCurrentPage}) => {
+  const sendButton = useRef<HTMLButtonElement>(null)
   const [searchField, setSearchField] = useState('')
   
   const searchTracks: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault()
+    
     const searchedTracks = items.filter(e => e[searchKey].toLocaleLowerCase().includes(searchField.toLocaleLowerCase()))
     setSearchedItems(searchedTracks)
+    setCurrentPage && setCurrentPage(0)
+  }
+
+  const resetSearch = () => {
+    setSearchField('')
+    
+    setTimeout(() => {
+      sendButton?.current?.click()
+    })
   }
 
   return (
     <div>
-      <form onSubmit={searchTracks}>
+      <form  onSubmit={searchTracks}>
         <input 
           className={`
             rounded-md border-2 border-gray-500
@@ -33,8 +45,17 @@ interface Props {
             px-4 py-2
             ml-2
           `}
+          ref={sendButton}
         >Search</button>
       </form>
+      <button 
+        className={`
+          rounded-md border-2 border-gray-500
+          px-4 py-2
+          ml-2
+        `}
+        onClick={resetSearch}
+      >Reset</button>
     </div>
   )
 }
