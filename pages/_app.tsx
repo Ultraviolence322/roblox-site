25dd1dbd6b041e5dc81823209bbd63a92da48b3d
Ultraviolence322 <segeichenkov18@gmail.com>
2021-11-29 16:22:23 +0300

    added head in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,23 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import React from 'react'
+import Head from 'next/head'
+
 import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <>
+    <Head>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;700&display=swap"
+        rel="stylesheet"
+      />
+    </Head>
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    </>
   )
 }
 
