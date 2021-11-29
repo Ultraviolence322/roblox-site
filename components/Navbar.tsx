6c98d41f8dea89a4ca63fad7ca74000a7c265d8b
Ultraviolence322 <segeichenkov18@gmail.com>
2021-11-29 15:27:06 +0300

    updated navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,26 +1,40 @@
 import { NextPage } from 'next'
 import React from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/dist/client/router'
 
 interface Props {
 
 }
 
 const Navbar: NextPage<Props> = () => {
+  const router = useRouter()
+  const getClassName = (path: string): string => {
+    let className = `
+      px-2 mx-2
+      text-white font-medium leading-8 text-2xl
+      border-b-4 
+      border-transparent
+      hover:border-white
+    `
+    if(router.pathname === path) className += 'border-white'
+
+    return className
+  }
   return (
-    <nav>
+    <nav className="bg-black p-8">
       <ul className="flex justify-center">
-        <li>
+        <li className={getClassName('/')}>
           <Link href="/">
             Top Music Codes
           </Link> 
         </li>
-        <li>
+        <li className={getClassName('/new')}>
           <Link href="/new">
             New Music Codes
           </Link>
         </li>
-        <li>
+        <li className={getClassName('/artists')}>
           <Link href="/artists">
             Artists
           </Link>
