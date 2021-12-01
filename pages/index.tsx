16421f7684854bf46ca7953c6ce3a9823288f6f0
Ultraviolence322 <segeichenkov18@gmail.com>
2021-12-01 10:32:45 +0300

    added logs in getstaticprops in index

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,19 +35,23 @@ const Home: NextPage<Props> = ({parsedSongs, accessToken}) => {
 
 
 export const getStaticProps: GetStaticProps = async ( ) => {
-  try {
-    const dev = process.env.NODE_ENV !== 'production';
-    const parsedSongs = await fetchAllSongs()
+  const parsedSongs = await fetchAllSongs()
+  const dev = process.env.NODE_ENV !== 'production';
+  console.log('dev', dev);
 
+  try {
     const responseFetchAccessToken = await fetch(`${dev ? process.env.DEV_URL : process.env.DEV_URL}/api/access-token`)
     const dataFetchAccessToken = await responseFetchAccessToken.json()
+
     return { 
       props: { parsedSongs, accessToken: dataFetchAccessToken.accessToken },
       revalidate: 3600,
     }
   } catch (error) {
+    console.log('error', error);
+     
     return {
-      props: { parsedSongs: '', accessToken: '' },
+      props: { parsedSongs, accessToken: '' },
       revalidate: 3600,
     }
   }
