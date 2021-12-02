dc3fb526601713d2fc0bd8d80561065d0ef12c10
Ultraviolence322 <segeichenkov18@gmail.com>
2021-12-02 10:29:42 +0300

    added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,49 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import React from 'react'
+import React, { useEffect } from 'react'
+import Script from 'next/script'
+import * as gtag from '../lib/gtag'
 
 import Layout from '../components/Layout'
+import { useRouter } from 'next/dist/client/router'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+  
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
