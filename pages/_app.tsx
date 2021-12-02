f6df015ede2b8467bdad29c0c0093bf93e64988e
Ultraviolence322 <segeichenkov18@gmail.com>
2021-12-02 18:47:02 +0300

    removed google analytics to back

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,48 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import React from 'react'
+import React, { useEffect } from 'react'
+import Script from 'next/script'
+import * as gtag from '../lib/gtag'
 
 import Layout from '../components/Layout'
-
+import { useRouter } from 'next/dist/client/router'
+import { Head } from 'next/document'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   
   return (
     <>
+    <Head>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+    </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
