9f0297b06ef258b7eff884f98423265ddb83f2a6
Ultraviolence322 <segeichenkov18@gmail.com>
2021-12-02 10:57:56 +0300

    added footer

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next'
+import React from 'react'
+
+const Footer:NextPage = () => {
+  return (
+    <footer className="bg-black p-8">
+      <p className="text-white text-center">Copyright © 2021 robloxmusiccodes.vercel.app</p>
+    </footer>
+  )
+}
+
+export default Footer
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from 'next'
 import React from 'react'
+import Footer from './Footer'
 import Navbar from './Navbar'
 
 interface Props {
@@ -9,8 +10,11 @@ interface Props {
 const Layout: NextPage<Props> = ({children}) => {
   return (
     <>
-      <Navbar />
-      <main className="mx-auto max-w-5xl px-4">{children}</main>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="mx-auto max-w-5xl px-4 flex-1">{children}</main>
+        <Footer />
+      </div>
     </>
   )
 }
